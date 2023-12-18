2d4475bffe8eacaddc4aec14cc8bea7c22a6f2d0
David <142182549+dmaiato@users.noreply.github.com>
2023-12-18 08:38:24 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -24,3 +24,9 @@ def wifi_connect(rede: str, senha: str) -> object:
     if not wifi.isconnected():
         raise
     return wifi
+
+def sensor_corrente(ina):
+    corrente = ina.current * 0.815
+    tensao = (corrente / 1000) * 9.4 
+    potencia = corrente * tensao
+    return corrente, tensao, potencia
